e605406bc6a54fd26e61d01a55266d5f2aebb01c
ommishra03 <143867637+ommishra03@users.noreply.github.com>
2024-06-23 13:13:56 +0530

    MyToken.sol

---
+++ Token.sol
@@ -0,0 +1,46 @@
+/*
+       REQUIREMENTS
+    1. Your contract will have public variables that store the details about your coin (Token Name, Token Abbrv., Total Supply)
+    2. Your contract will have a mapping of addresses to balances (address => uint)
+    3. You will have a mint function that takes two parameters: an address and a value. 
+       The function then increases the total supply by that number and increases the balance 
+       of the “sender” address by that amount
+    4. Your contract will have a burn function, which works the opposite of the mint function, as it will destroy tokens. 
+       It will take an address and value just like the mint functions. It will then deduct the value from the total supply 
+       and from the balance of the “sender”.
+    5. Lastly, your burn function should have conditionals to make sure the balance of "sender" is greater than or equal 
+       to the amount that is supposed to be burned.
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+contract MyToken {
+
+    //Public Variable
+    
+    string public tokenName = "OMKREATION";
+    string public tokenAbbrv = "KREAT";
+    uint public totalSupply = 0;
+
+    // Mapping from address to balances
+
+    mapping(address => uint) public balances;
+
+    // Mint function to create new token
+
+    function mint(address Add, uint value) public {
+        totalSupply += value;
+        balances[Add] += value;
+    }
+
+    // Burn function to destroy the token 
+
+    function burn(address Add, uint value) public {
+        if (balances[Add] >= value) {
+            totalSupply -= value;
+            balances[Add] -= value;
+        }
+    }
+}
+
